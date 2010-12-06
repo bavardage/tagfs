8b6fdf79e753875aa9aa452209ff62af39d1a40d
Ben Duffield <jebavarde@gmail.com>
2010-12-06 20:12:22 +0000

    so we got some very very basic fuse stuff working, i.e. there is something we can CD into, but pretty much nothing works...

--- tagfs.hs
+++ tagfs.hs
@@ -3,6 +3,9 @@ import Database.Redis.Monad hiding (connect)
 import Database.Redis.Redis (connect)
 import Control.Monad.Trans
 import Control.Monad (liftM)
+import System.Fuse
+import System.Environment (withArgs)
+import System.Posix.Files (ownerReadMode, ownerExecuteMode, ownerWriteMode, ownerModes, getFileStatus)
 
 ------------------------------------------------------------
 -- Utils
@@ -64,15 +67,89 @@ detag' (TempTag x ts) = (x, ts)
 ------------------------------------------------------------
 -- Fuse stuff
 ------------------------------------------------------------
+data FH = String
+directory, regularFile, emptyStat :: FileStat
+directory = emptyStat
+    { statEntryType = Directory
+    , statFileSize = 4096
+    , statBlocks = 1
+    , statFileMode = foldr1 unionFileModes [ ownerReadMode, ownerExecuteMode ]
+    }
 
+regularFile = emptyStat
+    { statEntryType = RegularFile
+    , statFileMode = foldr1 unionFileModes [ ownerReadMode, ownerWriteMode ]
+    }
+
+emptyStat = FileStat
+    { statEntryType = Unknown
+    , statFileMode = ownerModes
+    , statLinkCount = 0
+    , statFileOwner = 0
+    , statFileGroup = 0
+    , statSpecialDeviceID = 0
+    , statFileSize = 0
+    , statBlocks = 0
+    , statAccessTime = 0
+    , statModificationTime = 0
+    , statStatusChangeTime = 0
+    }
+-- in future use POSIX getFileStatus
+
+openDirectory :: FilePath -> IO Errno
+openDirectory f = return eOK
+
+readDirectory :: FilePath -> IO (Either Errno [(FilePath, FileStat)])
+readDirectory f = return $ Right dots
+    where dots = [(".", directory), ("..", directory)]
+
+getFileStat :: FilePath -> IO (Either Errno FileStat)
+getFileStat f = return $ Right regularFile
+
+--blatantly copied from FunionFS
+-- what the hell is the argument even?
+getFileSystemStats :: String -> IO (Either Errno FileSystemStats)
+getFileSystemStats str =
+  return $ Right FileSystemStats
+    { fsStatBlockSize  = 512
+    , fsStatBlockCount = 1
+    , fsStatBlocksFree = 1
+    , fsStatBlocksAvailable = 1
+    , fsStatFileCount  = 5      -- IS THIS CORRECT? PROBABLY NOT
+    , fsStatFilesFree  = 10     -- WHAT IS THIS? HELL IF I KNOW
+    , fsStatMaxNameLength = 255 -- SEEMS SMALL? YEAH IT DOES
+    }
+
+
+runFuse :: IO ()
+runFuse = do
+  withArgs ["/tmp/tagfs2/"] $ fuseMain fuseOps defaultExceptionHandler
+
+fuseOps :: FuseOperations FH
+fuseOps = defaultFuseOps { fuseOpenDirectory = openDirectory
+                         , fuseReadDirectory = readDirectory
+--                         , fuseGetFileStat = getFileStat
+
+                         -- , fuseOpen               = undefined
+                         -- , fuseFlush              = undefined
+                         -- , fuseRead               = undefined
+--                         , fuseGetFileSystemStats = getFileSystemStats
+
+                         -- Dummies to make FUSE happy.
+                         , fuseSetFileSize      = \_ _   -> return eOK
+                         , fuseSetFileTimes     = \_ _ _ -> return eOK
+                         , fuseSetFileMode      = \_ _   -> return eOK
+                         , fuseSetOwnerAndGroup = \_ _ _ -> return eOK
+                         }
 
 ------------------------------------------------------------
 -- Testy bleh stuff
 ------------------------------------------------------------
 main = do
   redis <- connect localhost defaultPort
-  runWithRedis redis go
+  runFuse
 
+go :: WithRedis m => m ()
 go = do
   addFileTag "pic1.jpg" "cat"
   addFileTag "pic1.jpg" "lol"
